f7c53db5c72fe27da3f78c7fdd4dfb3b38a6d033
meysam kazemi <93453960+meysam-kazemi@users.noreply.github.com>
2025-11-03 09:47:00 +0000

    add init file

---
+++ init.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = " "
+
+require("plugins")
+
+-- Basic options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.wrap = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.cmd[[colorscheme tokyonight-moon]]
+-- vim.opt.background = "light"
+-- vim.api.nvim_set_hl(50, "Normal", { bg = "none" })   -- transparent bg (optional)
